da2aeb8063ffc1939533d2cbc725c84287199a76
Bohati Mulyadi <bohatimulyadi99@gmail.com>
2023-10-29 05:25:07 +0700

    update

--- public/themes/modern/js/layar-liveupdate-monitor-antrian.js
+++ public/themes/modern/js/layar-liveupdate-monitor-antrian.js
@@ -10,7 +10,7 @@ current_volume = '';
 
 //WEBSOCKET
 var idSettingLayar = $('#id-setting-layar').text();
-var websocketURL = 'wss://10.20.30.252:8443?id=' + idSettingLayar;
+var websocketURL = 'wss://10.20.30.252:8443?id=4';
 
 function connectWebSocket() {
     var socket = new WebSocket(websocketURL);
